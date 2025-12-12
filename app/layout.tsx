ae7c059059cefec1f40c7550f1ae26aaa95900e1
Nick <cardsonbase@gmail.com>
2025-12-12 01:35:03 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
