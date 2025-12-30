37547d69a869f503c706d815d0e4e0893c908f49
CARDS Collectibles on Base <cardsonbase@gmail.com>
2025-12-29 22:26:30 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",  // If you have any
+    "./src/**/*.{js,ts,jsx,tsx}",    // If using src folder
   ],
   theme: {
     extend: {},
