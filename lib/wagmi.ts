70f424579cb1ab439a47f69b3fbf9e290c814bb3
CARDS Collectibles on Base <cardsonbase@gmail.com>
2026-02-27 12:15:15 -0600

    Update Builder Code

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -2,11 +2,8 @@ import { http, createConfig } from 'wagmi';
 import { base } from 'wagmi/chains';
 import { coinbaseWallet } from 'wagmi/connectors';
 import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector';
-import { Attribution } from 'ox/ercs/erc8021';  
 
-const DATA_SUFFIX = Attribution.toDataSuffix({
-  codes: [bc_xssv1xje],
-});
+const DATA_SUFFIX = '0x62635f7873737631786a650b0080218021802180218021802180218021' as `0x${string}`;
 
 export const wagmiConfig = createConfig({
   chains: [base],
